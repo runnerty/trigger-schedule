ca7ebbe719dc03aeebd671b8e7c8743a324862dd
carlos <carloscarmonaalcantara@gmail.com>
2017-09-05 10:09:20 +0200

    build: 0.0.1

---
+++ README.md
@@ -0,0 +1,18 @@
+# SCHEDULE trigger for [Runnerty]:
+
+### Configuration sample:
+```json
+{
+  "id": "schedule_default",
+  "type": "@runnerty-trigger-schedule"
+}
+```
+
+### Plan sample:
+```json
+{
+}
+```
+
+
+[Runnerty]: http://www.runnerty.io
---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const schedule = require("node-schedule");
+var Trigger = global.TriggerClass;
+
+class triggerSchedule extends Trigger {
+  constructor(chain, params) {
+    super(chain, params);
+  }
+
+  start() {
+    let _this = this;
+
+    // Create schedule Job with schedule_interval params:
+    _this.scheduleRepeater = schedule.scheduleJob(_this.params.schedule_interval, function () {
+      // Check if job must be canceled:
+      if ((new Date(_this.params.end_date)) < (new Date())) {
+        _this.scheduleRepeater.cancel();
+      }
+      // Start Chain:
+      _this.startChain()
+        .then(() => {})
+        .catch(err => {
+          _this.logger.error("startChain error (triggerSchedule):", err);
+        });
+
+    }.bind(null, _this));
+
+    // Schedule cancelation Job (end_date):
+    if (_this.params.end_date) {
+      _this.scheduleCancel = schedule.scheduleJob(new Date(_this.params.end_date), res => {
+        _this.scheduleRepeater.cancel();
+      });
+    }
+  }
+}
+
+module.exports = triggerSchedule;
